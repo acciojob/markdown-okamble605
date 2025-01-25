a2a1472fe96fe908f9ea282c19e5bba22cf3baf8
Omkar Baburao Kamble <121740559+okamble605@users.noreply.github.com>
2025-01-25 13:30:29 +0530

    Update App.js

--- src/components/App.js
+++ src/components/App.js
@@ -1,6 +1,6 @@
 import React, { useState, useEffect } from "react";
 import ReactMarkdown from "react-markdown";
-import "./App.css";
+import "./styles/App.css";
 
 const App = () => {
   const [markdown, setMarkdown] = useState("# Hello, Markdown!");
